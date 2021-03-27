754eb28d4f48002312aaf08c6f668e2f9993387e
alexrot15 <alexrotaru1595@gmail.com>
2021-03-27 16:33:29 +0200

    first deploy

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
 import os
-from flask import Flask, render_template, url_for
+import requests
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import ContactForm
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+API_URL = os.environ.get('API_URL')
+
+headers = {
+    'Authorization': os.environ.get('AUTHORIZATION')
+}
+
+
+def send_message(name, email, message):
+    api_params = {
+        'message': {
+            'nume': name,
+            'email': email,
+            'mesaj': message
+        }
+    }
+
+    response = requests.post(url=API_URL, json=api_params, headers=headers)
+    print(requests)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     form = ContactForm()
     if form.validate_on_submit():
-        pass
+        send_message(form.name.data, form.email.data, form.message.data)
+        flash('Your message has been sent!')
+        return redirect(url_for('home'))
     return render_template('home.html', form=form)
 
 
---
+++ static/js/main.js
@@ -0,0 +1,14 @@
+const section_ids = ['about', 'contact', 'education', 'projects', 'work-experience']
+const sections = document.querySelectorAll('.page-section');
+const links = document.querySelectorAll('.nav-link');
+
+links.forEach(link => {
+    link.addEventListener('click', e => {
+        const newDestination = document.querySelector(e.currentTarget.getAttribute('href'))
+        console.log(newDestination);
+        sections.forEach(section => {
+            section.classList.add('hide-section')
+        })
+        newDestination.classList.remove('hide-section')
+    })
+})
